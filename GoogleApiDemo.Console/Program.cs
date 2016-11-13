ed3e9d108553281e40a41e8aa3a0e856154be0bc
Nicholas Vella <vella.nicholas@outlook.com>
2016-11-13 12:45:20 +0100

    First commit

---
+++ GoogleApiDemo.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using System.Linq;
+
+namespace GoogleApiDemo.Console
+{
+    class Program
+    {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                ApiKey = "<API Key>",
+                ApplicationName = ApplicationName,
+
+            });
+
+            var events = service.Events.List("en-gb.mt#holiday@group.v.calendar.google.com").Execute();
+
+
+            foreach (var myEvent in events.Items.OrderByDescending(x => x.Start.Date))
+            {
+                System.Console.WriteLine(string.Format("Event: {0} Start: {1} End: {2}", myEvent.Summary, myEvent.Start.Date, myEvent.End.Date));
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
